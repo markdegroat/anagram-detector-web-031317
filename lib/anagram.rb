5bb2d18cae0b426ad7205c30a36790c40bed1f49
markdegroat <mhdegroat@gmail.com>
2017-03-21 14:55:43 -0400

    Final

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+
+class Anagram
+	attr_accessor :word_to_compare
+
+	def initialize(word)
+		@word_to_compare = word
+	end
+	def match(array)
+		array.select do |word|
+			word.scan(/\w/).sort == @word_to_compare.scan(/\w/).sort
+		end
+
+	end
+
+
+end
